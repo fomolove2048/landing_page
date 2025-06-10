3cfea9cb4be46b348ede07c58d14c7e6ecc4a7f2
xiaoqin001 <xiaoqin6787@outlook.com>
2025-06-09 22:39:26 -0700

    audit version1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Top from '@/components/Top';
 import Gameplay from '@/components/Gameplay';
 import Vision from '@/components/Vision';
+import Audit from '@/components/Audit';
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Top />
       <Gameplay />
       <Vision />
+      <Audit />
     </>
   );
 }
---
+++ src/components/Audit.js
@@ -0,0 +1,25 @@
+'use client'
+import styles from '@/styles/home.module.css'
+import beosin from '@/assets/images/beosin.png';
+import Image from 'next/image';
+import sonic_logo from '@/assets/images/sonic_logo.png'
+
+const Audit = ()=>{
+    const handleClick = ()=>{
+        window.open('https://beosin.com/audits/Fomoney_202505231107.pdf')
+    }
+    return (
+        <div className={styles.audit_bg}>
+            <div className={styles.audit_t1}>Audited by</div>
+            <div className={styles.audit_line}>
+                <Image src={beosin} alt='beosin' height={130} />
+                <div className={styles.audit_text} onClick={handleClick}>View Audit Report</div>
+            </div>
+            <div className={styles.audit_t1}>Built on</div>
+            <Image className={styles.sonic_logo} src={sonic_logo} alt='sonic_logo' height={100} />
+            <div className={styles.audit_bottom}></div>
+        </div>
+    )
+}
+
+export default Audit;
